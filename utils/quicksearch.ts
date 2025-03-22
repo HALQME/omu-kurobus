7c154d23924c281c451c32f3d9c1c5909677e5d4
HAL <68320771+HALQME@users.noreply.github.com>
2025-03-23 02:03:46 +0900

    クイック検索機能のエンコード処理を追加し、検索条件の厳密さを向上させるために閾値を調整。検索エンジンの柔軟性を向上させるために、最小マッチ文字数と文字距離を設定。

--- utils/quicksearch.ts
+++ utils/quicksearch.ts
@@ -1,8 +1,9 @@
 import Fuse from "fuse.js";
 import type { CourseEmbed, QuickSearchResult } from "@/types/schema";
+import { encode } from "./search-engine";
 
 export const quickSearch = (
-    normalizedSearchText: string,
+    query: string,
     courses: CourseEmbed[]
 ): QuickSearchResult[] => {
     const fuse = new Fuse(courses, {
@@ -10,26 +11,37 @@ export const quickSearch = (
             {
                 name: "name",
                 weight: 0.75,
+                getFn: (obj: CourseEmbed) => encode(obj.name),
             },
             {
                 name: "teachers",
                 weight: 0.6,
+                getFn: (obj: CourseEmbed) => encode(obj.teachers),
             },
             {
                 name: "id",
                 weight: 0.3,
+                getFn: (obj: CourseEmbed) => encode(obj.id),
             },
             {
                 name: "campus",
                 weight: 0.2,
+                getFn: (obj: CourseEmbed) => encode(obj.campus),
+            },
+            {
+                name: "semester",
+                weight: 0.1,
+                getFn: (obj: CourseEmbed) => encode(obj.semester ?? ""),
             },
         ],
-        threshold: 0.4,
+        threshold: 0.3,
         includeScore: true,
         shouldSort: true,
+        useExtendedSearch: true,
+        findAllMatches: true,
     });
 
-    const searchResults = fuse.search(normalizedSearchText);
+    const searchResults = fuse.search(encode(query));
 
     if (searchResults.length === 0) {
         console.log("No search results found");
